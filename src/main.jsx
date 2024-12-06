3adc4a0f4d6b6b0c88e303c815953c627d7b3a92
Pymich999 <bellorejoice839@gmail.com>
2024-12-06 19:33:48 +0100

    fixes made

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import { AuthProvider } from './context/AuthContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <div className="container mx-auto px-4">
+        <App />
+      </div>
     </AuthProvider>
   </StrictMode>,
 );
